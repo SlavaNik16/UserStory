9c0a16e28bb62992c08c9f3f5f75e70c3b25ba86
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-26 22:17:30 +0300

    Add files via upload

---
+++ UserStory4Tours/UserStory4Tours/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace UserStory4Tours
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Correction = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ChangeMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.DeliteMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.Program = new System.Windows.Forms.ToolStripMenuItem();
+            this.Info = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.AddTool = new System.Windows.Forms.ToolStripButton();
+            this.ChangeTool = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.DeliteTool = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.NumberToursStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.TotalAmount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.NumerToursSurcharges = new System.Windows.Forms.ToolStripStatusLabel();
+            this.TotalAmountSurcharges = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.Correction,
+            this.Program});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(810, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(48, 20);
+            this.File.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Correction
+            // 
+            this.Correction.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddMenu,
+            this.ChangeMenu,
+            this.toolStripSeparator1,
+            this.DeliteMenu});
+            this.Correction.Name = "Correction";
+            this.Correction.Size = new System.Drawing.Size(59, 20);
+            this.Correction.Text = "Правка";
+            // 
+            // AddMenu
+            // 
+            this.AddMenu.Name = "AddMenu";
+            this.AddMenu.Size = new System.Drawing.Size(128, 22);
+            this.AddMenu.Text = "Добавить";
+            this.AddMenu.Click += new System.EventHandler(this.AddMenu_Click);
+            // 
+            // ChangeMenu
+            // 
+            this.ChangeMenu.Enabled = false;
+            this.ChangeMenu.Name = "ChangeMenu";
+            this.ChangeMenu.Size = new System.Drawing.Size(128, 22);
+            this.ChangeMenu.Text = "Изменить";
+            this.ChangeMenu.Click += new System.EventHandler(this.ChangeMenu_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(125, 6);
+            // 
+            // DeliteMenu
+            // 
+            this.DeliteMenu.Enabled = false;
+            this.DeliteMenu.Name = "DeliteMenu";
+            this.DeliteMenu.Size = new System.Drawing.Size(128, 22);
+            this.DeliteMenu.Text = "Удалить";
+            this.DeliteMenu.Click += new System.EventHandler(this.DeliteMenu_Click);
+            // 
+            // Program
+            // 
+            this.Program.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Info});
+            this.Program.Name = "Program";
+            this.Program.Size = new System.Drawing.Size(94, 20);
+            this.Program.Text = "О программе";
+            // 
+            // Info
+            // 
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(148, 22);
+            this.Info.Text = "Информация";
+            this.Info.Click += new System.EventHandler(this.Info_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddTool,
+            this.ChangeTool,
+            this.toolStripSeparator2,
+            this.DeliteTool});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(810, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // AddTool
+            // 
+            this.AddTool.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AddTool.Name = "AddTool";
+            this.AddTool.Size = new System.Drawing.Size(63, 22);
+            this.AddTool.Text = "Добавить";
+            this.AddTool.Click += new System.EventHandler(this.AddTool_Click);
+            // 
+            // ChangeTool
+            // 
+            this.ChangeTool.Enabled = false;
+            this.ChangeTool.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ChangeTool.Name = "ChangeTool";
+            this.ChangeTool.Size = new System.Drawing.Size(65, 22);
+            this.ChangeTool.Text = "Изменить";
+            this.ChangeTool.Click += new System.EventHandler(this.ChangeTool_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // DeliteTool
+            // 
+            this.DeliteTool.Enabled = false;
+            this.DeliteTool.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DeliteTool.Name = "DeliteTool";
+            this.DeliteTool.Size = new System.Drawing.Size(55, 22);
+            this.DeliteTool.Text = "Удалить";
+            this.DeliteTool.Click += new System.EventHandler(this.DeliteTool_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(810, 414);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Направление";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.FillWeight = 110F;
+            this.Column2.HeaderText = "Дата вылета";
+            this.Column2.MinimumWidth = 50;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Кол-во ночей";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Стоимость за отдыхающего";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "Кол-во отдыхающих";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "Наличие Wi-Fi";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "Доплаты";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column8.HeaderText = "Общая стоимость";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NumberToursStatus,
+            this.TotalAmount,
+            this.NumerToursSurcharges,
+            this.TotalAmountSurcharges});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 441);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(810, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // NumberToursStatus
+            // 
+            this.NumberToursStatus.Name = "NumberToursStatus";
+            this.NumberToursStatus.Size = new System.Drawing.Size(89, 17);
+            this.NumberToursStatus.Text = "Кол-во туров 0";
+            // 
+            // TotalAmount
+            // 
+            this.TotalAmount.Name = "TotalAmount";
+            this.TotalAmount.Size = new System.Drawing.Size(94, 17);
+            this.TotalAmount.Text = "Общая сумма 0";
+            // 
+            // NumerToursSurcharges
+            // 
+            this.NumerToursSurcharges.Name = "NumerToursSurcharges";
+            this.NumerToursSurcharges.Size = new System.Drawing.Size(161, 17);
+            this.NumerToursSurcharges.Text = "Кол-во туров с доплатами 0";
+            // 
+            // TotalAmountSurcharges
+            // 
+            this.TotalAmountSurcharges.Name = "TotalAmountSurcharges";
+            this.TotalAmountSurcharges.Size = new System.Drawing.Size(135, 17);
+            this.TotalAmountSurcharges.Text = "Общая сумма доплат 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 463);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Горящие туры";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Correction;
+        private System.Windows.Forms.ToolStripMenuItem AddMenu;
+        private System.Windows.Forms.ToolStripMenuItem ChangeMenu;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem DeliteMenu;
+        private System.Windows.Forms.ToolStripMenuItem Program;
+        private System.Windows.Forms.ToolStripMenuItem Info;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton AddTool;
+        private System.Windows.Forms.ToolStripButton ChangeTool;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton DeliteTool;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel NumberToursStatus;
+        private System.Windows.Forms.ToolStripStatusLabel TotalAmount;
+        private System.Windows.Forms.ToolStripStatusLabel NumerToursSurcharges;
+        private System.Windows.Forms.ToolStripStatusLabel TotalAmountSurcharges;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+    }
+}
+
---
+++ UserStory4Tours/UserStory4Tours/models/Direction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStory4Tours.models
+{
+    /// <summary>
+    /// Направление
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// Турция
+        /// </summary>
+        Turkey,
+        /// <summary>
+        /// Испания
+        /// </summary>
+        Spain,
+        /// <summary>
+        /// Италия
+        /// </summary>
+        Italy,
+        /// <summary>
+        /// Франция
+        /// </summary>
+        France,
+        /// <summary>
+        /// Шушары
+        /// </summary>
+        Shushary,
+    }
+}
